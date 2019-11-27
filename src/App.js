db649b2ef0189680e169b5be1e5ec0cca7d9adaa
LauraPastor <laurapastorj@gmail.com>
2019-11-27 20:54:05 +0100

    laura update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Courses from './Courses';
 
 function App() {
   return (
--- src/Courses.js
+++ src/Courses.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import styled from "styled-components";
 
-function Courses(){
-    return (
-        <div className="Courses">
-          HTML & CSS
-        </div>
-      );}
+class Courses extends React.Component {
+  
 
+render() {
+  return (
+<div>
+  <StyledButton>Normal Button</StyledButton>
+  <StyledButton as="a" href="/">Link with Button styles</StyledButton>
+  <TomatoButton as="a" href="/">Link with Tomato Button styles</TomatoButton>
 
-
-
-
-
-
-
-
-
-
-
-
-
+</div>
+)
+ }
+  }
 
 export default Courses;
+
+const StyledButton = styled.button`
+display: inline-block;
+color: palevioletred;
+font-size: 1em;
+margin: 1em;
+padding: 0.25em 1em;
+border: 2px solid palevioletred;
+border-radius: 3px;
+display: block;
+`;
+const TomatoButton = styled(StyledButton)`
+color: tomato;
+border-color: tomato;
+`;
